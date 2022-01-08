9253d914b37eb82dbfec40a08c28b13c703cc327
sadelcarpio <sadelcarpioa@gmail.com>
2022-01-08 10:18:04 -0500

    Se corrigio doble 'card' en result

--- index.js
+++ index.js
@@ -46,32 +46,3 @@ async function runNst() {
   result.dispose();
 
 }
-
-
-// async function app() {
-//     console.log('Cargando mobilenet ...');
-
-//     // Cargar modelo:
-//     net = await mobilenet.load();
-//     console.log('¡Modelo cargado exitosamente!');
-
-//     // Realizar predicción de la cámara web
-//     const webcam = await tf.data.webcam(webcamElement);
-//     while (true) {
-//         const img = await webcam.capture();
-//         const result = await net.classify(img);
-    
-//         document.getElementById('console').innerText = `
-//           prediction: ${result[0].className}\n
-//           probability: ${result[0].probability}
-//         `;
-//         // Dispose the tensor to release the memory.
-//         img.dispose();
-    
-//         // Give some breathing room by waiting for the next animation frame to
-//         // fire.
-//         await tf.nextFrame();
-//       }
-//     }
-
-// app();
